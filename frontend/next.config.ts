de3d72897fc639c4b76fc26efc7901fe4e9e5fe3
vansh-vagadia <vanshvagadia1602@gmail.com>
2025-09-10 09:24:46 +0530

    config changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,12 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  eslint:{
-    ignoreDuringBuilds:true
-  }
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
-export default nextConfig;
+module.exports= nextConfig;
